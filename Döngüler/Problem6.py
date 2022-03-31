69bfecaf44109c571f052326ddcc1e1ce95ae9ea
Zaid Ahmed <zaidasim232@gmail.com>
2022-03-31 15:15:09 +0300

    List Comprehension

---
+++ Döngüler/Problem6.py
@@ -0,0 +1,24 @@
+#Burada mantık yürüterek ve list comprehension kullanarak 1'den 100'e kadar olan 
+#sayılardan sadece çift sayıları bir listeye atmayı yapmayı çalışın.
+
+list_comp = list() 
+
+for i in range(1,100):
+    list_comp.append(i)
+
+cift_sayilar = [x for x in list_comp if x%2 == 0 ]
+print(cift_sayilar)
+
+liste = [[1,2,3],[4,5,6,7,8],[9,10,11,12,13,14,15]]
+liste2 = list()
+for i in liste:
+    for x in i:
+        #print("x:",x)
+        liste2.append(x)
+print(liste2)
+    
+# List Comprehension 
+
+liste = [[1,2,3],[4,5,6,7,8],[9,10,11,12,13,14,15]]
+liste2 = [x for i in liste for x in i] # Biraz karmaşık
+print(liste2)
